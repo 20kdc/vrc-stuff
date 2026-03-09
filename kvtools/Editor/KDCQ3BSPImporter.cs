25b7e056c108898bd79810c9f09d050f682322f1
t20kdc <asdd2808@gmail.com>
2026-03-09 13:48:35 +0000

    q3bsp: Add stub importer so we know where to put the real one when it's done

---
+++ kvtools/Editor/KDCQ3BSPImporter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+using System.IO;
+
+namespace KDCVRCTools {
+	[ScriptedImporter(1, "bsp")]
+	public class KDCQ3BSPImporter : ScriptedImporter {
+		public override void OnImportAsset(AssetImportContext ctx) {
+			GameObject mapGO = new GameObject("map");
+			ctx.AddObjectToAsset("main obj", mapGO);
+			ctx.SetMainObject(mapGO);
+		}
+	}
+}
